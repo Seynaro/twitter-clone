d73a478475065d76fb45a63d08b8e8886e13b324
Khamzat <xamzatseynaro@gmail.com>
2021-04-15 23:04:35 +0300

    add ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import SignIn from './pages/SingIn'
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<SignIn/>
+		</div>
+	);
 }
 
-export default App;
+export default App
