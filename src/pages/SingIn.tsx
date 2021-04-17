49f1200675754ca2828da16004e288420bf5e5da
Khamzat <xamzatseynaro@gmail.com>
2021-04-17 21:12:13 +0300

    add layout into Home/SingIn

---
+++ src/pages/SingIn.tsx
@@ -0,0 +1,122 @@
+import React from "react"
+import {Button, makeStyles, Typography} from "@material-ui/core";
+import TwitterIcon from '@material-ui/icons/Twitter';
+import SearchIcon from '@material-ui/icons/Search';
+import PeopleIcon from '@material-ui/icons/PeopleOutline';
+import MessageIcon from '@material-ui/icons/ModeCommentOutlined';
+
+const useStylesSignIn = makeStyles((theme) => ({
+	wrapper: {
+		display: 'flex',
+		height: 'calc(100vh - 84px)',
+	},
+	blueSide: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#71C9F8',
+		flex: '0 0 50%',
+		overflow: 'hidden',
+		position: 'relative',
+	},
+	blueSideBigIcon: {
+		position: 'absolute',
+		left: '50%',
+		top: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: '350%',
+		height: '350%',
+	},
+	blueSideListInfo: {
+		position: 'relative',
+		listStyle: 'none',
+		padding: 0,
+		margin: 0,
+		width: 380,
+		'& h6': {
+			display: 'flex',
+			alignItems: 'center',
+			color: 'white',
+			fontWeight: 700,
+			fontSize: 20,
+		},
+	},
+	blueSideListInfoItem: {
+		marginBottom: 40,
+	},
+	blueSideListInfoIcon: {
+		fontSize: 32,
+		marginRight: 15,
+	},
+	loginSide: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		flex: '0 0 50%',
+	},
+	loginSideTwitterIcon: {
+		fontSize: 45,
+	},
+	loginSideWrapper: {
+		width: 380,
+	},
+	loginSideTitle: {
+		fontWeight: 700,
+		fontSize: 32,
+		marginBottom: 60,
+		marginTop: 20,
+	}
+}))
+
+export const SignIn: React.FC = (): React.ReactElement => {
+	const classes = useStylesSignIn()
+
+	const [visibleModal, setVisibleModal] = React.useState<'signIn' | 'signUp'>()
+
+	const handleClickOpenSignIn = (): void => {
+		setVisibleModal('signIn')
+	}
+
+	const handleClickOpenSignUp = (): void => {
+		setVisibleModal('signUp')
+	}
+
+	const handleCloseModal = (): void => {
+		setVisibleModal(undefined)
+	}
+
+	return (
+		<div className={classes.wrapper}>
+			<section className={classes.blueSide}>
+				<TwitterIcon color="primary" className={classes.blueSideBigIcon}/>
+				<ul className={classes.blueSideListInfo}>
+					<li className={classes.blueSideListInfoItem}>
+						<Typography variant='h6'>
+							<SearchIcon className={classes.blueSideListInfoIcon}/>
+							Читайте о том, что вам интересно.</Typography></li>
+					<li className={classes.blueSideListInfoItem}>
+						<Typography variant='h6'>
+							<PeopleIcon className={classes.blueSideListInfoIcon}/>
+							Узнайте, о чем говорят в мире.</Typography></li>
+					<li className={classes.blueSideListInfoItem}><Typography variant='h6'>
+						<MessageIcon className={classes.blueSideListInfoIcon}/>
+						Присоединяйтесь к общению.</Typography></li>
+				</ul>
+			</section>
+			<section className={classes.loginSide}>
+				<div className={classes.loginSideWrapper}>
+					<TwitterIcon color="primary" className={classes.loginSideTwitterIcon}/>
+					<Typography className={classes.loginSideTitle} gutterBottom variant="h4">Узнайте, что происходит в
+						мире прямо сейчас</Typography>
+					<Typography><b>Присоединяйтесь к Твиттеру прямо сейчас!</b></Typography>
+					<br/>
+					<Button style={{marginBottom: 20}} variant="contained" color="primary"
+							fullWidth>Зарегистрироваться</Button>
+					<Button variant="outlined" color="primary" fullWidth>Войти</Button>
+				</div>
+			</section>
+		</div>
+	)
+}
+
+export default SignIn
