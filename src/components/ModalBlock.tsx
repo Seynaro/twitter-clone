efc299028602faf2ab85580a31c638c2ea2ae1d0
Khamzat <xamzatseynaro@gmail.com>
2021-04-19 22:25:32 +0300

    supplement layout in ModalBlock

--- src/components/ModalBlock.tsx
+++ src/components/ModalBlock.tsx
@@ -1,59 +1,23 @@
-import React from "react";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import { Dialog } from "@material-ui/core";
-import IconButton from "@material-ui/core/IconButton";
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
-interface ModalProps {
-	title: string
-	children: React.ReactNode
-}
 
-export const ModalBlock: React.FC<ModalProps> = ({title, children}: ModalProps): React.ReactElement => {
-	const [open, setOpen] = React.useState(false)
+export const ModalBlock = () => {
 
-	const handleClickOpen = () => {
-		setOpen(true)
-	}
-
-	const handleClose = () => {
-		setOpen(false)
-	}
 
 	return (
-		<Dialog open={open}
-	onClose = {handleClose}
-	aria - Labelledby = 'form-dialog-'
-		< DialogTitle
-	id = 'form-dialog-title' >
-	<IconButton onClick = {handleClose}
-	color = "secondary"
-	aria - Label = ""
-		< CloseIcon
-	style = {
-	{
-		fontSize: }
-}
-	color = "secondary" / >
-		</IconButton>
-	Войти
-	в
-	Твиттер
-	< /DialogTitle>
-	< DialogContent >
-	<FormControl component = "fieldset"
-	fullWidth >
-	<FormGroup aria - label = "position"
-	row >
-	<TextField className = {classnames.loginSideField}
-	autoFocus
-	id = "email"
-	label = "E-Mail"
-	InputLabelProps = {
-	{
-		shrink: true,
-	}
-}
-	variant = "filled"
-	type = "email"
-)
-}
+		<Dialog  aria-labelledby="form-dialog-title">
+			<DialogTitle id="form-dialog-title">
+				<IconButton  color="secondary" aria-label="close">
+					<CloseIcon style={{ fontSize: 26 }} color="secondary" />
+				</IconButton>
+				{title}
+			</DialogTitle>
+			<DialogContent>{children}</DialogContent>
+		</Dialog>
+	);
+};
