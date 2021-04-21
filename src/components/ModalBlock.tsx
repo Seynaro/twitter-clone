e6307827b7f6d3fc648f0340660c5975f5d066ba
Khamzat <xamzatseynaro@gmail.com>
2021-04-21 20:47:49 +0300

    add interface/visible mod in ModalBlock.tsx

--- src/components/ModalBlock.tsx
+++ src/components/ModalBlock.tsx
@@ -1,23 +1,40 @@
-import React from 'react';
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
+import React from 'react'
+import Dialog from '@material-ui/core/Dialog'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import IconButton from '@material-ui/core/IconButton'
+import CloseIcon from '@material-ui/icons/Close'
+import { useStylesSignIn } from '../pages/SingIn'
+
+interface ModalBlockProps {
+	title: string
+	children: React.ReactNode
+	classes?: ReturnType<typeof useStylesSignIn>
+	visible?: boolean
+	onClose: () => void
+}
+
+export const ModalBlock: React.FC<ModalBlockProps> = ({
+														  title,
+														  children,
+														  onClose,
+														  visible = false
+													  }: ModalBlockProps): React.ReactElement | null => {
 
 
-export const ModalBlock = () => {
-
+	if (!visible) {
+		return null
+	}
 
 	return (
-		<Dialog  aria-labelledby="form-dialog-title">
+		<Dialog open={visible} onClose={onClose} aria-labelledby="form-dialog-title">
 			<DialogTitle id="form-dialog-title">
-				<IconButton  color="secondary" aria-label="close">
-					<CloseIcon style={{ fontSize: 26 }} color="secondary" />
+				<IconButton color="secondary" aria-label="close">
+					<CloseIcon style={{fontSize: 26}} color="secondary"/>
 				</IconButton>
 				{title}
 			</DialogTitle>
 			<DialogContent>{children}</DialogContent>
 		</Dialog>
-	);
-};
+	)
+}
